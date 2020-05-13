0768ff4663577c5f25c65bae96aa1927d5006191
unknown <you@example.com>
2020-05-13 20:44:53 +0200

    Variabelen en datatype  6

---
+++ 2-Variabelen-en-datatype/taak06/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+	<?php
+        $s = "ja";
+        $i = 16;
+        $f = 1.65;
+        $b = true;
+        $n = null;
+        echo gettype($s) . "<br>";
+        echo gettype($i) . "<br>";
+        echo gettype($f) . "<br>";
+        echo gettype($b) . "<br>";
+        echo gettype($n) . "<br>";
+	?>
+</body>
+
+</html>
