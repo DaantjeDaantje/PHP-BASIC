0fc447e16c695feebdeb788f2a0d36bf660ade78
unknown <you@example.com>
2020-05-13 20:53:29 +0200

    Variabelen en datatype 8

---
+++ 2-Variabelen-en-datatype/taak08/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+	<?php
+        $voornaam1 = "Hana";
+        $voornaam2 = "Janro";
+        $voornaam3 = "Daniëlle";
+
+        echo "<li>$voornaam1</li><li>$voornaam2</li><li>$voornaam3</li>";
+	?>
+</body>
+
+</html>
