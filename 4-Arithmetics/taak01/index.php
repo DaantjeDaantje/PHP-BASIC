cb8c4346696e04729b5fef60198ff7f1219c9d82
DaantjeDaantje <danielleroelofsma@gmail.com>
2020-05-14 21:12:28 +0200

    Arithmetics 1

---
+++ 4-Arithmetics/taak01/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+	<?php
+        $getal1 = 69;
+        $getal2 = 14;
+        echo $getal1 + $getal2 . "</br>";
+        echo $getal1 - $getal2 . "</br>";
+        echo $getal1 * $getal2 . "</br>";
+        echo $getal1 /   $getal2 . "</br>";
+	?>
+</body>
+
+</html>
