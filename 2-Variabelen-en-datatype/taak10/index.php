552a33993f05b2809db4525318faab9dbaf45c12
DaantjeDaantje <danielleroelofsma@gmail.com>
2020-05-14 20:31:59 +0200

    Variabelen en datatype 10

---
+++ 2-Variabelen-en-datatype/taak10/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+	<?php
+        $item = "<li>banaan</li>";
+        $item .= "<li>iets</li>";
+        $item .= "<li>ja</li>";
+        echo "<ul>$item</ul";
+	?>
+</body>
+
+</html>
