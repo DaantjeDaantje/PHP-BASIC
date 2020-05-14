8e7d0173c18e7dde515cea8cb07afb58ea6d63be
DaantjeDaantje <danielleroelofsma@gmail.com>
2020-05-14 22:08:31 +0200

    Conditional Statements 2

---
+++ 5-Conditional_Statements/taak02/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+	<?php
+        $test1 = 10;
+        $test2 = 6;
+        if($test1 == $test2){
+            echo "gelijk </br>";
+        }
+
+        if($test1 != $test2){
+            echo "ongelijk </br>";
+        }
+
+        if($test1 === $test2){
+            echo "gelijk </br>";
+        }
+
+        if($test1 > $test2){
+            echo "gelijk </br>";
+        }
+
+        if($test1 < $test2){
+            echo "gelijk </br>";
+        }
+
+        if($test1 >= $test2){
+            echo "gelijk </br>";
+        }
+
+        if($test1 <= $test2){
+            echo "gelijk </br>";
+        }
+	?>
+</body>
+
+</html>
