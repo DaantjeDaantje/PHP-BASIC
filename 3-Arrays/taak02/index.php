e2f168b3882988d2abc064e5e041dd733571caaa
DaantjeDaantje <danielleroelofsma@gmail.com>
2020-05-14 21:05:39 +0200

    Arrays 2

---
+++ 3-Arrays/taak02/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+	<?php
+        $myDogsNames = array("Mikey", "Fluffy", "Ringo");
+        echo $myDogsNames[0] . "</br>";
+        echo $myDogsNames[1] . "</br>";
+        echo $myDogsNames[2] . "</br>";
+        $myDogsNames[2] = "Simba";
+        echo $myDogsNames[2] . "</br>";
+	?>
+</body>
+
+</html>
