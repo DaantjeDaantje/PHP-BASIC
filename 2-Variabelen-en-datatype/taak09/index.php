af83c0e1725a8dfca61f4067849c4f20f77d77ef
DaantjeDaantje <danielleroelofsma@gmail.com>
2020-05-14 20:09:27 +0200

    Variabelen en datatype 9

---
+++ 2-Variabelen-en-datatype/taak09/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+	<?php
+		    $voornaam = "Daniëlle";
+			$leeftijd = 16;
+			echo "Jouw naam is " . $voornaam . " en je bent " . $leeftijd . " jaar oud.";
+	?>
+</body>
+
+</html>
