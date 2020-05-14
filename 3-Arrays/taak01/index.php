fb8aa1e232d287b1efdbe055c8113cb8b63d8934
DaantjeDaantje <danielleroelofsma@gmail.com>
2020-05-14 20:54:14 +0200

    Arrays 1

---
+++ 3-Arrays/taak01/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+	<?php
+        $myMovies = array("Kono Subarashii movie", "Frozen", "Frozen 2");
+        echo "<ul><li>$myMovies[0]</li><li>$myMovies[1]</li><li>$myMovies[2]</li></ul>"
+	?>
+</body>
+
+</html>
