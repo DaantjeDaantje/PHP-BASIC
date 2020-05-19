b868c7afa08cea9bcfbadbe23a7ad317a46924df
DaantjeDaantje <danielleroelofsma@gmail.com>
2020-05-19 19:51:13 +0200

    Include Require 1

---
+++ 7-Include-Require/taak01/includes/menu.php
@@ -0,0 +1,8 @@
+<body>
+    <?php
+    $item1 = "iets";
+    $item2 = "iets";
+    $item3 = "iets";
+    echo "</u><li>$item1</li><li>$item2</li><li>$item3</li></ul>";
+	?>
+</body>
---
+++ 7-Include-Require/taak01/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+</head>
+
+<body>
+    <?php
+    include "includes/menu.php";
+    ?>
+</body>
+
+</html>
